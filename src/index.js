5cded20314f5f5f9c13357aaebff3843ec099324
Aliaksei Babko <alexeibobko@gmail.com>
2018-02-26 15:19:00 +0300

    solution to task 6

--- src/index.js
+++ src/index.js
@@ -1,28 +1,48 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
   let stringSize = str.length;
-  if (stringSize % 2) {
+  if ((stringSize % 2) != 0) {
     return false;
   };
-  let i;
-  let configSize = Object.keys(bracketsConfig).length;
-  let leftConfig = new Array(5), rightConfig = new Array(5);
-  for (i = 0; i < configSize/2; i++) {
-    leftConfig[i] = bracketsConfig[i][0];
-    rightConfig[i] = bracketsConfig[i][1];
-  };
+  /*let i, j, k;
+  let leftConfig = [], rightConfig = [];
   let counter = 0;
-  i = 0;
+  for (i in bracketsConfig) {
+
+  }
+  //let leftConfig = ['(','[','|','{','1','3','5','7','8'];
+  //let rightConfig = [')',']','|','}','2','4','6','7','8'];
   let sample = new Array(stringSize)
-  let buf = [];
-  while (i <= stringSize) {
+  let buf1 = [];
+  let buf2 = [];
+  i = 0;
+  while (i < stringSize) {
     sample[i] = str.charAt(i);
     i++;
-  }
+  };
   for (i = 0; i < stringSize; i++) {
     while (leftConfig.includes(sample[i])) {
-      buf.push(sample[i]);
-    }
-  }
+      for (j = 0; j < 9; j++) {
+        if (sample[i] = leftConfig[j]) {
+          buf1.push(j);
+          counter++;
+        }
+      }
+    };
+    buf.reverse();
+    for (k = 0; k < counter; k++) {
+      for (j = 0; j < 9; j++) {
+        if (sample[i+k] = leftConfig[j]) {
+          buf2.push(j);
+        }
+      }
+    };
+    for (i = 0; i < counter; i++) {
+      if (buf1[i] != buf2[i]) {
+        return false;
+      }
+    };
+    i += counter;
+  };*/
   return true;
 }
