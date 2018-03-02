e0478f8c617d02f88e4ff021909e529754a0a6f7
Aliaksei Babko <alexeibobko@gmail.com>
2018-03-03 00:20:34 +0300

    solution to task 6

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
 let configSize = bracketsConfig.length;
+let strSize = str.length;
 let leftConfig = [], rightConfig = [], stack = [];
-let ch;
-let expected;
+let ch, expected;
 for (let i = 0; i< configSize/2; i++) {
   leftConfig [i] = bracketsConfig [i][0];
   rightConfig [i] = bracketsConfig [i][1];
@@ -11,28 +11,23 @@ for (let i = 0; i< configSize/2; i++) {
 if (str.length % 2) {
   return false;
 }
-if (rightConfig.indexOf(str[0]) == leftConfig.indexOf(str[2])) {
+if (leftConfig.indexOf(str[0]) == rightConfig.indexOf(str[2])) {
   return false;
 }
-for (let i=0; i < str.length; i++) {
+for (let i=0; i < strSize; i++) {
   ch = str[i];
   if (leftConfig.includes(ch) > -1) {
     stack.push();
   }
   else if (rightConfig.includes(ch) > -1) {
     expected = leftConfig[rightConfig.indexOf(ch)];
-    if (stack.length == 0 || stack.pop() != expected) {
+    if (stack.length === 0 || stack.pop() != expected) {
       return false;
     }
     else {
       continue;
     }
-  }  
-if (stack.length == 0) {
-  return true;
-    }
-    else {
-      return false;
-    }
   }
+}
+return (stack.length === 0);
 }
